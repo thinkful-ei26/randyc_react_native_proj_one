2a1497c366d28088e37594bd86f69fb2c36e8a9d
R-Cole <randy@goodglue.com>
2019-02-04 11:38:36 -0700

    setup

---
+++ components/hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react';
+
+export default class Hello extends Component{
+
+  render() {
+
+    return (
+       <Text>OH BOY</Text>
+    )
+  };
+ 
+}
